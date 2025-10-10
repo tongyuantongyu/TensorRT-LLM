88946807a4f4835bad3416797c5b94dc0004c2b0
Mina Huai <121143971+MinaHuai@users.noreply.github.com>
2025-10-09 19:30:39 -0700

    rebase to the updated verl

--- tensorrt_llm/llmapi/llm.py
+++ tensorrt_llm/llmapi/llm.py
@@ -186,7 +186,7 @@ class BaseLLM:
         self.mpi_session = self.args.mpi_session
 
         if self.args.parallel_config.is_multi_gpu:
-            if os.getenv("RAY_LOCAL_RANK") is None and get_device_count(
+            if os.getenv("RAY_LOCAL_WORLD_SIZE") is None and get_device_count(
             ) < self.args.parallel_config.world_size_per_node:
                 raise RuntimeError(
                     f"Only {get_device_count()} GPUs are available, but {self.args.parallel_config.world_size} are required."
--- tensorrt_llm/llmapi/llm_args.py
+++ tensorrt_llm/llmapi/llm_args.py
@@ -1635,7 +1635,7 @@ class BaseLlmArgs(StrictBaseModel):
     @field_validator("gpus_per_node", mode='before')
     @classmethod
     def validate_gpus_per_node(cls, v, info):
-        if os.getenv("RAY_LOCAL_RANK") is not None:
+        if os.getenv("RAY_LOCAL_WORLD_SIZE") is not None:
             return info.data.get("tensor_parallel_size")
         if v is None:
             logger.warning(
